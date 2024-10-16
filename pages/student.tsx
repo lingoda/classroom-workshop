b4bb5a1f89dd28f355cad37eb295e292ef264d2a
Oleksii Myronenko <mironenkoalexey@gmail.com>
2024-10-16 14:35:32 +0300

    Init quiz module

--- pages/student.tsx
+++ pages/student.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { initStudentQuizClient } from "../src/quiz";
+
 export const Student = () => {
+  useQuiz();
+
   return <div>Student</div>;
+};
+
+const useQuiz = () => {
+  useEffect(() => {
+    initStudentQuizClient();
+  }, []);
 };
 
 export default Student;
---
+++ src/quiz/questionsLib.ts
@@ -0,0 +1,31 @@
+import mockedQuestions from "./mock.json";
+
+type QuestionItem = {
+  text: string;
+  answers: string[];
+  correctAnswer: string;
+};
+
+export const getQuestionDataByIndex = (index: number): QuestionItem => {
+  const matchedQuestion = mockedQuestions.quiz[index];
+
+  if (!matchedQuestion) {
+    throw new Error(`Question with index ${index} not found`);
+  }
+
+  const correctAnswerItem = Object.entries(matchedQuestion.answers).find(
+    ([answer, isCorrect]) => isCorrect === true
+  );
+  if (!correctAnswerItem) {
+    throw new Error(
+      `Correct answer for question "${matchedQuestion.question}" not found`
+    );
+  }
+  const correctAnswer = correctAnswerItem[0];
+
+  return {
+    text: matchedQuestion.question,
+    answers: Object.keys(matchedQuestion.answers),
+    correctAnswer,
+  };
+};
