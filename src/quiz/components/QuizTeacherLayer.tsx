ad81f2a2275b347c723ca8fa33ed426568dc41bf
radkra <radek.kraszewski@gmail.com>
2024-10-17 10:40:56 +0200

    fake quiz participants amount

--- src/quiz/components/QuizTeacherLayer.tsx
+++ src/quiz/components/QuizTeacherLayer.tsx
@@ -1,17 +1,23 @@
-import {  QuizLayerContainer } from "./uiBlocks";
-import {  startQuizQuestion } from "../sendMessage";
-import { useTeacherSideQuiz } from "../hooks";
-import { QuizTeacherLayout } from "./QuizTeacherLayout";
+import { QuizLayerContainer } from './uiBlocks';
+import { startQuizQuestion } from '../sendMessage';
+import { useTeacherSideQuiz } from '../hooks';
+import { QuizTeacherLayout } from './QuizTeacherLayout';
 
 export const QuizTeacherLayer = () => {
-  const { userName, currentQuestion, currentQuestionIndex, questionParticipantsAmount, submittedParticipantsAmount } = useTeacherSideQuiz();
+  const {
+    userName,
+    currentQuestion,
+    currentQuestionIndex,
+    questionParticipantsAmount,
+    submittedParticipantsAmount,
+  } = useTeacherSideQuiz();
 
   return (
     <QuizLayerContainer>
       <QuizTeacherLayout
         currentQuestion={currentQuestion}
         currentQuestionIndex={currentQuestionIndex}
-        questionParticipantsAmount={questionParticipantsAmount}
+        questionParticipantsAmount={1}
         submittedParticipantsAmount={submittedParticipantsAmount}
         startQuizQuestion={startQuizQuestion}
         userName={userName}
--- src/quiz/hooks.tsx
+++ src/quiz/hooks.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import { getRandomName } from "./utils";
-import { saveCorrectAnswer, useQuizStore } from "./store";
-import { currentQuestionIndexSelector } from "./store";
-import { currentQuestionSelector } from "./store";
-import { sendCorrectAnswer } from "./sendMessage";
+import { useEffect, useState } from 'react';
+import { getRandomName } from './utils';
+import { saveCorrectAnswer, useQuizStore } from './store';
+import { currentQuestionIndexSelector } from './store';
+import { currentQuestionSelector } from './store';
+import { sendCorrectAnswer } from './sendMessage';
 
 export const useUserPersistentName = () => {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
 
   useEffect(() => {
     setName(getRandomName());
@@ -23,21 +23,24 @@ export const useTeacherSideQuiz = () => {
   console.log('currentQuestionIndex', currentQuestionIndex);
   const questionParticipants = currentQuestion?.participants || [];
   console.log('questionParticipants', questionParticipants);
-  const questionParticipantsAmount = currentQuestion?.participants.length ?? 0;
+  const questionParticipantsAmount = 1;
   console.log('questionParticipantsAmount', questionParticipantsAmount);
   const submittedParticipants = questionParticipants.filter(
     (participant) => participant.submittedAnswer !== null
   );
   const submittedParticipantsAmount = submittedParticipants.length;
 
+  console.log('questionParticipantsAmount', questionParticipantsAmount);
+  console.log('submittedParticipantsAmount', submittedParticipantsAmount);
+
   useEffect(() => {
     if (
-      currentQuestion?.status === "active" &&
+      currentQuestion?.status === 'active' &&
       questionParticipantsAmount > 0 &&
       submittedParticipantsAmount >= questionParticipantsAmount
     ) {
       if (!currentQuestion.correctAnswer) {
-        throw new Error("Teacher is expected to have a correct answer");
+        throw new Error('Teacher is expected to have a correct answer');
       }
       saveCorrectAnswer(
         currentQuestion.question,
@@ -60,5 +63,5 @@ export const useTeacherSideQuiz = () => {
     currentQuestionIndex,
     questionParticipantsAmount,
     submittedParticipantsAmount,
-  }
-}
+  };
+};
